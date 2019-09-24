dd14b09a5af9b4a1b7835d72307f1d461b26d0a3
jjabrane <jabrane.pro@gmail.com>
2019-09-23 23:46:12 -0400

    First commit

---
+++ assets/src/app.js
@@ -0,0 +1,14 @@
+require("../node_modules/jquery/dist/jquery.min.js");
+require ("../node_modules/popper.js/dist/umd/popper.min.js");
+require ("../node_modules/bootstrap/dist/js/bootstrap.min.js");
+
+const Vue = require('vue');
+new Vue({
+  el:'#app',
+  data:{
+   seen: true
+  },
+  method:{
+
+  }
+});
