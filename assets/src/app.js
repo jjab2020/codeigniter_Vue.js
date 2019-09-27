e800b242a68b3fb70a1626da11b045640361e06e
jjabrane <jabrane.pro@gmail.com>
2019-09-27 00:20:00 -0400

    Add sbadmin 2 template

--- assets/src/app.js
+++ assets/src/app.js
@@ -1,6 +1,8 @@
 require("../node_modules/jquery/dist/jquery.min.js");
 require ("../node_modules/popper.js/dist/umd/popper.min.js");
 require ("../node_modules/bootstrap/dist/js/bootstrap.min.js");
+require ("../node_modules/startbootstrap-sb-admin-2/vendor/jquery-easing/jquery.easing.min.js");
+require ("../node_modules/startbootstrap-sb-admin-2/js/sb-admin-2.js");
 
 const Vue = require('vue');
 
