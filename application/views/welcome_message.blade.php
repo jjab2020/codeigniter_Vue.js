275632ce526601c10cba167a5c66a96a33d6cc88
jjabrane <jabrane.pro@gmail.com>
2019-09-27 23:42:01 -0400

    Add sb admin 2 to codeigniter with laravel mix

--- application/views/welcome_message.blade.php
+++ application/views/welcome_message.blade.php
@@ -1,168 +1,62 @@
-<?php
-defined('BASEPATH') OR exit('No direct script access allowed');
-?><!DOCTYPE html>
-<html lang="en">
-<head>
-	<meta charset="utf-8">
-	<title>Welcome to CodeIgniter</title>
-	<link rel="stylesheet" type="text/css" href="{{base_url()}}assets/dist/app.css">
-	<style type="text/css">
-		::selection { background-color: #f07746; color: #fff; }
-		::-moz-selection { background-color: #f07746; color: #fff; }
-		body {
-			background-color: #fff;
-			margin: 40px auto;
-			max-width: 1024px;
-			font: 16px/24px normal "Helvetica Neue",Helvetica,Arial,sans-serif;
-			color: #808080;
-		}
-		a {
-			color: #dd4814;
-			background-color: transparent;
-			font-weight: normal;
-			text-decoration: none;
-		}
-		a:hover {
-			color: #97310e;
-		}
-		
-		h1 img {
-			display: block;
-		}
-		h2 {
-			color:#404040;
-			margin:0;
-			padding:0 0 10px 0;
-		}
-		code {
-			font-family: Consolas, Monaco, Courier New, Courier, monospace;
-			font-size: 13px;
-			background-color: #f5f5f5;
-			border: 1px solid #e3e3e3;
-			border-radius: 4px;
-			color: #002166;
-			display: block;
-			margin: 14px 0 14px 0;
-			padding: 12px 10px 12px 10px;
-		}
-		#body {
-			margin: 0 15px 0 15px;
-			min-height: 96px;
-		}
-		p {
-			margin: 0 0 10px;
-			padding:0;
-		}
-		p.footer {
-			text-align: right;
-			font-size: 12px;
-			border-top: 1px solid #d0d0d0;
-			line-height: 32px;
-			padding: 0 10px 0 10px;
-			margin: 20px 0 0 0;
-			background:#8ba8af;
-			color:#fff;
-		}
-		#container {
-			margin: 10px;
-			border: 1px solid #d0d0d0;
-			box-shadow: 0 0 8px #d0d0d0;
-			border-radius: 4px;
-		}
-	</style>
-</head>
-
-<body>
-
-	<div id="container">
-		<div id="app">
-			<h1><span>@{{ msg }} </span></h1>	
-		</div>
-		<h1>Hello my friend {{ $name }} from codeigniter</h1>
-		@for($a=1;$a<=3;$a++)
-		   <p>{{$a}}</p>
-		@endfor
-		<div class="alert alert-dismissible alert-warning">
-			<button type="button" class="close" data-dismiss="alert">&times;</button>
-			<h4 class="alert-heading">Warning!</h4>
-			<p class="mb-0"><i class="fas fa-user"></i> Best check yo self, you're not looking too good. Nulla vitae elit libero, a pharetra augue. Praesent commodo cursus magna, <a href="#" class="alert-link">vel scelerisque nisl consectetur et</a>.</p>
-		</div>
-		<div class="container">
-			<table class="table table-hover">
-				<thead>
-					<tr>
-						<th scope="col">Type</th>
-						<th scope="col">Column heading</th>
-						<th scope="col">Column heading</th>
-						<th scope="col">Column heading</th>
-					</tr>
-				</thead>
-				<tbody>
-					<tr class="table-active">
-						<th scope="row">Active</th>
-						<td>Column content</td>
-						<td>Column content</td>
-						<td>Column content</td>
-					</tr>
-					<tr>
-						<th scope="row">Default</th>
-						<td>Column content</td>
-						<td>Column content</td>
-						<td>Column content</td>
-					</tr>
-					<tr class="table-primary">
-						<th scope="row">Primary</th>
-						<td>Column content</td>
-						<td>Column content</td>
-						<td>Column content</td>
-					</tr>
-					<tr class="table-secondary">
-						<th scope="row">Secondary</th>
-						<td>Column content</td>
-						<td>Column content</td>
-						<td>Column content</td>
-					</tr>
-					<tr class="table-success">
-						<th scope="row">Success</th>
-						<td>Column content</td>
-						<td>Column content</td>
-						<td>Column content</td>
-					</tr>
-					<tr class="table-danger">
-						<th scope="row">Danger</th>
-						<td>Column content</td>
-						<td>Column content</td>
-						<td>Column content</td>
-					</tr>
-					<tr class="table-warning">
-						<th scope="row">Warning</th>
-						<td>Column content</td>
-						<td>Column content</td>
-						<td>Column content</td>
-					</tr>
-					<tr class="table-info">
-						<th scope="row">Info</th>
-						<td>Column content</td>
-						<td>Column content</td>
-						<td>Column content</td>
-					</tr>
-					<tr class="table-light">
-						<th scope="row">Light</th>
-						<td>Column content</td>
-						<td>Column content</td>
-						<td>Column content</td>
-					</tr>
-					<tr class="table-dark">
-						<th scope="row">Dark</th>
-						<td>Column content</td>
-						<td>Column content</td>
-						<td>Column content</td>
-					</tr>
-				</tbody>
-			</table> 
-		</div>
-		<p class="footer">Page rendered in <strong>{elapsed_time}</strong> seconds. <?php echo  (ENVIRONMENT === 'development') ?  'CodeIgniter Version <strong>' . CI_VERSION . '</strong>' : '' ?></p>
-	</div>
-	<script type="text/javascript" src={{base_url()}}assets/dist/app.js ></script>
-</body>
-</html>
+@extends('layouts.main') 
+@section('content')
+    <!-- DataTales Example -->
+    <div class="card shadow mb-4">
+        <div class="card-header py-3">
+          <h6 class="m-0 font-weight-bold text-primary">DataTables Example</h6>
+        </div>
+            <div class="card-body">
+              <div class="table-responsive">
+                <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
+                  <thead>
+                    <tr>
+                      <th>Name</th>
+                      <th>Position</th>
+                      <th>Office</th>
+                      <th>Age</th>
+                      <th>Start date</th>
+                      <th>Salary</th>
+                    </tr>
+                  </thead>
+ 
+                  <tbody>
+                    <tr>
+                      <td>Tiger Nixon</td>
+                      <td>System Architect</td>
+                      <td>Edinburgh</td>
+                      <td>61</td>
+                      <td>2011/04/25</td>
+                      <td>$320,800</td>
+                    </tr>
+                    <tr>
+                      <td>Garrett Winters</td>
+                      <td>Accountant</td>
+                      <td>Tokyo</td>
+                      <td>63</td>
+                      <td>2011/07/25</td>
+                      <td>$170,750</td>
+                    </tr>
+                    <tr>
+                      <td>Ashton Cox</td>
+                      <td>Junior Technical Author</td>
+                      <td>San Francisco</td>
+                      <td>66</td>
+                      <td>2009/01/12</td>
+                      <td>$86,000</td>
+                    </tr>
+                    <tr>
+                      <td>Cedric Kelly</td>
+                      <td>Senior Javascript Developer</td>
+                      <td>Edinburgh</td>
+                      <td>22</td>
+                      <td>2012/03/29</td>
+                      <td>$433,060</td>
+                    </tr>
+ 
+                  </tbody>
+                </table>
+              </div>
+            </div>
+    </div>
+ 
+@endsection
--- assets/src/app.js
+++ assets/src/app.js
@@ -1,15 +1,18 @@
-require("../node_modules/jquery/dist/jquery.min.js");
-require ("../node_modules/popper.js/dist/umd/popper.min.js");
-require ("../node_modules/bootstrap/dist/js/bootstrap.min.js");
-require ("../node_modules/startbootstrap-sb-admin-2/vendor/jquery-easing/jquery.easing.min.js");
-require ("../node_modules/startbootstrap-sb-admin-2/js/sb-admin-2.js");
+import 'jquery/dist/jquery.min.js';
+import 'popper.js/dist/umd/popper.min.js';
+import 'bootstrap/dist/js/bootstrap.min.js';
+import 'startbootstrap-sb-admin-2/vendor/jquery-easing/jquery.easing.min.js';
+import 'startbootstrap-sb-admin-2/js/sb-admin-2.js';
+import 'startbootstrap-sb-admin-2/vendor/datatables/jquery.dataTables.min.js';
+import 'jquery-ui/ui/widgets/datepicker.js';
+import 'select2';
 
 const Vue = require('vue');
 
 new Vue({
   el:'#app',
   data:{
-   msg: "Hello everbody from Vue.js!!!!!"
+   msg: "SB Admin!"
   },
   method:{
 
